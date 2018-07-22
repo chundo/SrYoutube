0edbfdd38e9229e89d0f9fd351408a698e94f630
chundo <segundo.espana1@gmail.com>
2018-07-22 03:21:35 -0500

    Layout and form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
   def index
+    if params['url']
+      puts '------------------------------------'
+      if params['format_d'] == 'mp4'
+        @file = YoutubeDL.download params['url'], output: 'public/download/some_file.mp4'
+      elsif params['format_d'] == 'mp3'
+        @file = YoutubeDL.download params['url'], output: 'public/download/some_file.mp3'
+      end
+      puts '------------------------------------'
+    end
   end
 
+  def download(file)
+    #.split('public/download/')
+    send_file file, filename: "video.mp4", type: "application/mp4"
+  end
+  
   def list
   end
 
